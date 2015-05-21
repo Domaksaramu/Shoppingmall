6d395f27a94677f0f8941c0e4c1f1b5eb0c1b03f
limjewon <jewon945@gmail.com>
2015-05-21 02:52:38 -0700

    input tables

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
-	validates :categoty, :inclusion => { :in =>["Man","Woman","Accessory"], :message => "you must choose one of them, Man, Woman and Accessory."}
+	validates :category, :inclusion => { :in =>["Man","Woman","Accessory"], :message => "you must choose one of them, Man, Woman and Accessory."}
 	validates :title, :presence => { :message => "Title is needed."}
 end
